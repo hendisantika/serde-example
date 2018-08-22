624b0a1df54817ec7502d57ccb3fd3ef7cbdde55
Hendi Santika <hendisantika@gmail.com>
2018-08-23 06:39:28 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/serdeexample/SerdeExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.serdeexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerdeExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SerdeExampleApplication.class, args);
+    }
+}
