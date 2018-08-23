d7db558b804ac19e1468d40ded866e84d6c2f689
Hendi Santika <hendisantika@gmail.com>
2018-08-23 07:03:36 +0700

    #11 - Bug Fiz Serialized User Entity

--- src/main/java/com/hendisantika/serdeexample/entity/SerializedUser.java
+++ src/main/java/com/hendisantika/serdeexample/entity/SerializedUser.java
@@ -1,11 +1,6 @@
 package com.hendisantika.serdeexample.entity;
 
-import org.springframework.data.annotation.Id;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * Created by IntelliJ IDEA.
